ea88b6199781579d292ed24bb75e6cdd419cb366
bonxatiwat <bon.atiwat@gmail.com>
2024-04-09 15:31:01 +0700

    feat: setup payment service

--- modules/payment/paymentHandler/paymentQueue.go
+++ modules/payment/paymentHandler/paymentQueue.go
@@ -6,14 +6,14 @@ import (
 )
 
 type (
-	PaymentHttpHandlerService interface{}
+	PaymentQueueHandlerService interface{}
 
-	paymentHttpHandler struct {
+	paymentQueueHandler struct {
 		cfg            *config.Config
 		paymentUsecase paymentUsecase.PaymentUsecaseService
 	}
 )
 
-func NewPaymentHttpHandler(cfg *config.Config, paymentUsecase paymentUsecase.PaymentUsecaseService) PaymentHttpHandlerService {
-	return &paymentHttpHandler{cfg, paymentUsecase}
+func NewPaymentQueueHandler(cfg *config.Config, paymentUsecase paymentUsecase.PaymentUsecaseService) PaymentQueueHandlerService {
+	return &paymentQueueHandler{cfg, paymentUsecase}
 }
--- modules/payment/paymentUsecase/paymentUsecase.go
+++ modules/payment/paymentUsecase/paymentUsecase.go
@@ -1,1 +1,15 @@
 package paymentUsecase
+
+import "github.com/bonxatiwat/bonx-shop-tutorial/modules/payment/paymentRepository"
+
+type (
+	PaymentUsecaseService interface{}
+
+	paymentUsecase struct {
+		paymentRepository paymentRepository.PaymentRepositoryService
+	}
+)
+
+func NewPaymentUsecase(paymentRepository paymentRepository.PaymentRepositoryService) PaymentUsecaseService {
+	return &paymentUsecase{paymentRepository}
+}
