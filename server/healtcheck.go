559dad11096ce8adaa4a24c07868bde3022276f9
bonxatiwat <bon.atiwat@gmail.com>
2024-04-09 18:37:10 +0700

    feat: create health check service

--- server/healtcheck.go
+++ server/healtcheck.go
@@ -1,1 +1,20 @@
 package server
+
+import (
+	"net/http"
+
+	"github.com/bonxatiwat/bonx-shop-tutorial/pkg/response"
+	"github.com/labstack/echo/v4"
+)
+
+type healthCheck struct {
+	App    string `json:"app"`
+	Stauts string `json:"status"`
+}
+
+func (s *server) healthCheckService(c echo.Context) error {
+	return response.SuccessResponse(c, http.StatusOK, &healthCheck{
+		App:    s.cfg.App.Name,
+		Stauts: "OK",
+	})
+}
