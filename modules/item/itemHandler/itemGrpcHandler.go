a9f50d7cc3a55479e34d74258f883e1e5c77ed03
bonxatiwat <bon.atiwat@gmail.com>
2024-04-09 14:42:30 +0700

    feat: setup item service

--- modules/item/itemHandler/itemGrpcHandler.go
+++ modules/item/itemHandler/itemGrpcHandler.go
@@ -1,1 +1,13 @@
 package itemHandler
+
+import "github.com/bonxatiwat/bonx-shop-tutorial/modules/item/itemUsecase"
+
+type (
+	itemGrpcHandler struct {
+		itemUsecase itemUsecase.ItemUsecaseService
+	}
+)
+
+func NewItemGrpcHandler(itemUsecase itemUsecase.ItemUsecaseService) *itemGrpcHandler {
+	return &itemGrpcHandler{itemUsecase}
+}
--- modules/item/itemHandler/itemHttpHandler.go
+++ modules/item/itemHandler/itemHttpHandler.go
@@ -1,1 +1,19 @@
 package itemHandler
+
+import (
+	"github.com/bonxatiwat/bonx-shop-tutorial/config"
+	"github.com/bonxatiwat/bonx-shop-tutorial/modules/item/itemUsecase"
+)
+
+type (
+	ItemHttpHandlerService interface{}
+
+	itemHttpHandler struct {
+		cfg         *config.Config
+		itemUsecase itemUsecase.ItemUsecaseService
+	}
+)
+
+func NewItemHttpHandler(cfg *config.Config, itemUsecase itemUsecase.ItemUsecaseService) ItemHttpHandlerService {
+	return &itemHttpHandler{cfg, itemUsecase}
+}
