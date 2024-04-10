848e640c41f8cfb5224a66a37bc7ef270cd2b972
bonxatiwat <bon.atiwat@gmail.com>
2024-04-10 18:19:12 +0700

    feat: initialize protobuf auth

--- modules/auth/authHandler/authGrpcHandler.go
+++ modules/auth/authHandler/authGrpcHandler.go
@@ -1,13 +1,29 @@
 package authHandler
 
-import "github.com/bonxatiwat/bonx-shop-tutorial/modules/auth/authUsecase"
+import (
+	"context"
+
+	authPb "github.com/bonxatiwat/bonx-shop-tutorial/modules/auth/authPb"
+	"github.com/bonxatiwat/bonx-shop-tutorial/modules/auth/authUsecase"
+)
 
 type (
 	authGrpcHandler struct {
+		authPb.UnimplementedAuthGrpcServiceServer
 		authUsecase authUsecase.AuthUsecaseService
 	}
 )
 
 func NewAuthGrpcHandler(authUsecase authUsecase.AuthUsecaseService) *authGrpcHandler {
-	return &authGrpcHandler{authUsecase}
+	return &authGrpcHandler{
+		authUsecase: authUsecase,
+	}
+}
+
+func (g *authGrpcHandler) CrednetialSearch(ctx context.Context, req *authPb.CredentialSearchReq) (*authPb.CredentialSearchRes, error) {
+	return nil, nil
+}
+
+func (g *authGrpcHandler) RolesCount(ctx context.Context, req *authPb.RolesCountReq) (*authPb.RolesCountRes, error) {
+	return nil, nil
 }
