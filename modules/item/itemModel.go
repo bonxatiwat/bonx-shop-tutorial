82617794ee9d64b049fda3fba950b98351faa750
bonxatiwat <bon.atiwat@gmail.com>
2024-04-10 17:46:14 +0700

    feat: domain building service

--- modules/item/itemModel.go
+++ modules/item/itemModel.go
@@ -1,1 +1,36 @@
 package item
+
+import "github.com/bonxatiwat/bonx-shop-tutorial/modules/models"
+
+type (
+	CreateItemReq struct {
+		Title    string  `json:"title" validate:"required,max=64"`
+		Price    float64 `json:"price" validate:"price"`
+		ImageUrl string  `json:"image_url" validate:"required,max=255"`
+		Damage   int     `json:"damage" validate:"required,max=255"`
+	}
+
+	ItemShowCase struct {
+		ItemId   string `json:"item_id"`
+		Title    string `json:"title"`
+		Price    string `json:"price"`
+		Damage   int    `json:"damage"`
+		ImageUrl string `json:"image_url"`
+	}
+
+	ItemSearchReq struct {
+		Title string `json:"title"`
+		models.PaginateReq
+	}
+
+	ItemUpdateReq struct {
+		Title    string  `json:"title" validate:"required,max=64"`
+		Price    float64 `json:"price" validate:"required"`
+		ImageUrl string  `json:"image_url" validate:"required,max=255"`
+		Damage   int     `json:"damage" validate:"required,max=255"`
+	}
+
+	EnableOrDisableItemReq struct {
+		UsageStauts bool `json:"staus"`
+	}
+)
