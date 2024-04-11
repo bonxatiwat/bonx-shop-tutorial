feccd4aa4b866a00eed2e459bd7115f86276927b
bonxatiwat <bon.atiwat@gmail.com>
2024-04-11 11:53:45 +0700

    feat: initialize protobuf inventory

--- modules/inventory/inventoryHandler/inventoryGrpcHandler.go
+++ modules/inventory/inventoryHandler/inventoryGrpcHandler.go
@@ -1,13 +1,23 @@
 package inventoryHandler
 
-import "github.com/bonxatiwat/bonx-shop-tutorial/modules/inventory/inventoryUsecase"
+import (
+	"context"
+
+	inventoryPb "github.com/bonxatiwat/bonx-shop-tutorial/modules/inventory/inventoryPb"
+	"github.com/bonxatiwat/bonx-shop-tutorial/modules/inventory/inventoryUsecase"
+)
 
 type (
 	inventoryGrpcHandler struct {
 		inventoryUsecase inventoryUsecase.InventoryUsecaseService
+		inventoryPb.UnsafeInventoryGrpcServiceServer
 	}
 )
 
 func NewInvertoryGrpcHandler(inventoryUsecase inventoryUsecase.InventoryUsecaseService) *inventoryGrpcHandler {
-	return &inventoryGrpcHandler{inventoryUsecase}
+	return &inventoryGrpcHandler{inventoryUsecase: inventoryUsecase}
+}
+
+func (g *inventoryGrpcHandler) IsAvaliableToSell(ctx context.Context, req *inventoryPb.IsAvaliableToSellReq) (*inventoryPb.IsAvaliableToSellRes, error) {
+	return nil, nil
 }
