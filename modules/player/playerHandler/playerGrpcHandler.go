420812721926b0bbb2e575b425916f1076fd62d9
bonxatiwat <bon.atiwat@gmail.com>
2024-04-11 11:26:35 +0700

    feat: initialize protobuf player

---
+++ modules/player/playerHandler/playerGrpcHandler.go
@@ -0,0 +1,31 @@
+package playerHandler
+
+import (
+	"context"
+
+	playerPb "github.com/bonxatiwat/bonx-shop-tutorial/modules/player/playerPb"
+	"github.com/bonxatiwat/bonx-shop-tutorial/modules/player/playerUsecase"
+)
+
+type (
+	playerGrpcHandler struct {
+		playerPb.UnimplementedPlayerGrpcServiceServer
+		playerUsecase playerUsecase.PlayerUsecaseService
+	}
+)
+
+func NewPlayerGrpcHandler(playerUsecase playerUsecase.PlayerUsecaseService) *playerGrpcHandler {
+	return &playerGrpcHandler{playerUsecase: playerUsecase}
+}
+
+func (g *playerGrpcHandler) CredentialSearch(ctx context.Context, req *playerPb.CredentialSearchReq) (*playerPb.PlayerProfile, error) {
+	return nil, nil
+}
+
+func (g *playerGrpcHandler) FindOnePlayerProfileToRefresh(ctx context.Context, req *playerPb.FindOnePlayerProfileToRefreshReq) (*playerPb.PlayerProfile, error) {
+	return nil, nil
+}
+
+func (g *playerGrpcHandler) GetPlayerSavingAccount(ctx context.Context, req *playerPb.GetPlayerSavingAccountReq) (*playerPb.GetPlayerSavingAccountRes, error) {
+	return nil, nil
+}
