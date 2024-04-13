365d8105e6cc588df254949817b74f413007a869
bonxatiwat <bon.atiwat@gmail.com>
2024-04-13 14:34:13 +0700

    feat: register player

--- modules/player/playerHandler/playerHttpHandler.go
+++ modules/player/playerHandler/playerHttpHandler.go
@@ -1,12 +1,21 @@
 package playerHandler
 
 import (
+	"context"
+	"net/http"
+
 	"github.com/bonxatiwat/bonx-shop-tutorial/config"
+	"github.com/bonxatiwat/bonx-shop-tutorial/modules/player"
 	"github.com/bonxatiwat/bonx-shop-tutorial/modules/player/playerUsecase"
+	"github.com/bonxatiwat/bonx-shop-tutorial/pkg/request"
+	"github.com/bonxatiwat/bonx-shop-tutorial/pkg/response"
+	"github.com/labstack/echo/v4"
 )
 
 type (
-	PlayerHttpHandlerService interface{}
+	PlayerHttpHandlerService interface {
+		CreatePlayer(c echo.Context) error
+	}
 
 	playerHttpHandler struct {
 		cfg           *config.Config
@@ -16,4 +25,23 @@ type (
 
 func NewPlayerHttpHandler(cfg *config.Config, playerUsecase playerUsecase.PlayerUsecaseService) PlayerHttpHandlerService {
 	return &playerHttpHandler{cfg, playerUsecase}
+}
+
+func (h *playerHttpHandler) CreatePlayer(c echo.Context) error {
+	ctx := context.Background()
+
+	wrapper := request.ContextWrapper(c)
+
+	req := new(player.CreatePlayerReq)
+
+	if err := wrapper.Bind(req); err != nil {
+		return response.ErrorRespose(c, http.StatusBadRequest, err.Error())
+	}
+
+	res, err := h.playerUsecase.CreatePlayer(ctx, req)
+	if err != nil {
+		return response.ErrorRespose(c, http.StatusBadRequest, err.Error())
+	}
+
+	return response.SuccessResponse(c, http.StatusCreated, res)
 }
--- modules/player/playerUsecase/playerUsecase.go
+++ modules/player/playerUsecase/playerUsecase.go
@@ -1,11 +1,19 @@
 package playerUsecase
 
 import (
+	"context"
+	"errors"
+
+	"github.com/bonxatiwat/bonx-shop-tutorial/modules/player"
 	"github.com/bonxatiwat/bonx-shop-tutorial/modules/player/playerRepository"
+	"github.com/bonxatiwat/bonx-shop-tutorial/pkg/utils"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type (
-	PlayerUsecaseService interface{}
+	PlayerUsecaseService interface {
+		CreatePlayer(pctx context.Context, req *player.CreatePlayerReq) (string, error)
+	}
 
 	playerUsecase struct {
 		playerRepository playerRepository.PlayerRepositoryService
@@ -14,4 +22,32 @@ type (
 
 func NewPlayerUsecase(playerRepository playerRepository.PlayerRepositoryService) PlayerUsecaseService {
 	return &playerUsecase{playerRepository}
+}
+
+func (u *playerUsecase) CreatePlayer(pctx context.Context, req *player.CreatePlayerReq) (string, error) {
+	if !u.playerRepository.IsUniquePlayer(pctx, req.Email, req.Username) {
+		return "", errors.New("error: email or username already exist")
+	}
+
+	// Hashing password
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", errors.New("error: failed to hash password")
+	}
+
+	// Insert one player
+	playerId, err := u.playerRepository.InsertOnePlayer(pctx, &player.Player{
+		Email:     req.Email,
+		Username:  req.Username,
+		Password:  string(hashedPassword),
+		CreatedAt: utils.LocalTime(),
+		UpdatedAt: utils.LocalTime(),
+		PlayerRoles: []player.PlayerRole{
+			{
+				RoleTitle: "player",
+				RoleCode:  0,
+			},
+		},
+	})
+	return playerId.Hex(), nil
 }
