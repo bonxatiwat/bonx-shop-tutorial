1d214294d14d84ecb791a00f23d8c22cb46e8cbf
bonxatiwat <bon.atiwat@gmail.com>
2024-04-14 15:15:12 +0700

    feat: get player saving account

--- modules/player/playerRepository/playerRepository.go
+++ modules/player/playerRepository/playerRepository.go
@@ -20,6 +20,7 @@ type (
 		InsertOnePlayer(pctx context.Context, req *player.Player) (primitive.ObjectID, error)
 		FindOnePlayerProfile(pctx context.Context, playerId string) (*player.PlayerProfileBson, error)
 		InsertOnePlayerTransaction(pctx context.Context, req *player.PlayerTransaction) error
+		GetPlayerSavingAccount(pctx context.Context, playerId string) (*player.PlayerSavingAccount, error)
 	}
 
 	playerRepository struct {
@@ -28,7 +29,7 @@ type (
 )
 
 func NewPlayerRepository(db *mongo.Client) PlayerRepositoryService {
-	return &playerRepository{db}
+	return &playerRepository{db: db}
 }
 
 func (r *playerRepository) playerDbConn(pctx context.Context) *mongo.Database {
@@ -118,4 +119,70 @@ func (r *playerRepository) InsertOnePlayerTransaction(pctx context.Context, req 
 	log.Printf("Result: InsertOnePlayerTransaction: %v", result.InsertedID)
 
 	return nil
+}
+
+func (r *playerRepository) GetPlayerSavingAccount(pctx context.Context, playerId string) (*player.PlayerSavingAccount, error) {
+	ctx, cancel := context.WithTimeout(pctx, 10*time.Second)
+	defer cancel()
+
+	db := r.playerDbConn(ctx)
+	col := db.Collection("player_transactions")
+
+	// filter := bson.A{
+	// 	bson.D{{Key: "$match", Value: bson.D{{Key: "player_id", Value: "player:661b7dff523a48d559ba2ee9"}}}},
+	// 	bson.D{
+	// 		{Key: "$group",
+	// 			Value: bson.D{
+	// 				{Key: "_id", Value: "$player_id"},
+	// 				{Key: "balance", Value: bson.D{{Key: "$sum", Value: "$amount"}}},
+	// 			},
+	// 		},
+	// 	},
+	// 	bson.D{
+	// 		{Key: "$project",
+	// 			Value: bson.D{
+	// 				{Key: "player_id", Value: "$_id"},
+	// 				{Key: "_id", Value: 0},
+	// 				{Key: "balance", Value: 1},
+	// 			},
+	// 		},
+	// 	},
+	// }
+
+	filter := bson.A{
+		bson.D{{"$match", bson.D{{"player_id", playerId}}}},
+		bson.D{
+			{"$group",
+				bson.D{
+					{"_id", "$player_id"},
+					{"balance", bson.D{{"$sum", "$amount"}}},
+				},
+			},
+		},
+		bson.D{
+			{"$project",
+				bson.D{
+					{"player_id", "$_id"},
+					{"_id", 0},
+					{"balance", 1},
+				},
+			},
+		},
+	}
+
+	cursors, err := col.Aggregate(ctx, filter)
+	if err != nil {
+		return nil, errors.New("error: failed to get player saving account")
+	}
+
+	result := new(player.PlayerSavingAccount)
+
+	for cursors.Next(ctx) {
+		if err := cursors.Decode(result); err != nil {
+			log.Printf("Error: GetPlayerSavingAccount: %s", err.Error())
+			return nil, errors.New("error: failed to get player saving account")
+		}
+	}
+
+	return result, nil
 }
