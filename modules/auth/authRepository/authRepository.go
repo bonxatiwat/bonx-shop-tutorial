35ce41e0b5738a2e5cc63020748ecd07bed14cb9
bonxatiwat <bon.atiwat@gmail.com>
2024-06-24 09:36:47 +0700

    feat: logout service

--- modules/auth/authRepository/authRepository.go
+++ modules/auth/authRepository/authRepository.go
@@ -26,6 +26,7 @@ type (
 		RefreshToken(cfg *config.Config, claims *jwtauth.Claims) string
 		FindOnePlayerProfileToRefresh(pctx context.Context, grpcUrl string, req *playerPb.FindOnePlayerProfileToRefreshReq) (*playerPb.PlayerProfile, error)
 		UpdateOnePlayerCredential(pctx context.Context, credentialId string, req *auth.UpdateRefreshTokenReq) error
+		DeleteOnePlayerCredential(pctx context.Context, credentialId string) (int64, error)
 	}
 
 	authRepository struct {
@@ -154,4 +155,20 @@ func (r *authRepository) UpdateOnePlayerCredential(pctx context.Context, credent
 	}
 
 	return nil
+}
+
+func (r *authRepository) DeleteOnePlayerCredential(pctx context.Context, credentialId string) (int64, error) {
+	ctx, cancel := context.WithTimeout(pctx, 10*time.Second)
+	defer cancel()
+
+	db := r.authDbConn(ctx)
+	col := db.Collection("auth")
+
+	result, err := col.DeleteOne(ctx, bson.M{"_id": utils.ConvertToObjectId(credentialId)})
+	if err != nil {
+		log.Printf("Error: DeleteOnePlayerCredential failed: %s", err.Error())
+		return -1, errors.New("error: delete one player credential failed")
+	}
+	log.Printf("DeleteOnePlayerCredential result: %d", result)
+	return result.DeletedCount, nil
 }
--- modules/auth/authUsecase/authUsecase.go
+++ modules/auth/authUsecase/authUsecase.go
@@ -18,8 +18,9 @@ import (
 
 type (
 	AuthUsecaseService interface {
-		Login(ptcx context.Context, cfg *config.Config, req *auth.PlayerLoginReq) (*auth.ProfileIntercepter, error)
+		Login(pctx context.Context, cfg *config.Config, req *auth.PlayerLoginReq) (*auth.ProfileIntercepter, error)
 		RefreshToken(pctx context.Context, cfg *config.Config, req *auth.RefreshTokenReq) (*auth.ProfileIntercepter, error)
+		Logout(pctx context.Context, credentialId string) (int64, error)
 	}
 
 	authUsecase struct {
@@ -147,4 +148,8 @@ func (u *authUsecase) RefreshToken(pctx context.Context, cfg *config.Config, req
 			UpdatedAt:    credential.UpdatedAt.In(loc),
 		},
 	}, nil
+}
+
+func (u *authUsecase) Logout(pctx context.Context, credentialId string) (int64, error) {
+	return u.authRepository.DeleteOnePlayerCredential(pctx, credentialId)
 }
