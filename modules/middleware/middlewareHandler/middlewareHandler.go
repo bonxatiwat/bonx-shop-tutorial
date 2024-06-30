9022116f11ab6a86737e9d836f8f5f792bfdf1ec
bonxatiwat <bon.atiwat@gmail.com>
2024-06-30 16:00:24 +0700

    feat: player id param validation

--- modules/middleware/middlewareHandler/middlewareHandler.go
+++ modules/middleware/middlewareHandler/middlewareHandler.go
@@ -14,6 +14,7 @@ type (
 	MiddlewareHandlerService interface {
 		JwtAuthorization(next echo.HandlerFunc) echo.HandlerFunc
 		RbacAuthorization(next echo.HandlerFunc, expected []int) echo.HandlerFunc
+		PlayerIdParamValidation(next echo.HandlerFunc) echo.HandlerFunc
 	}
 
 	middlewareHandler struct {
@@ -44,6 +45,16 @@ func (h *middlewareHandler) JwtAuthorization(next echo.HandlerFunc) echo.Handler
 func (h *middlewareHandler) RbacAuthorization(next echo.HandlerFunc, expected []int) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		newCtx, err := h.middlewareUsecase.RbacAuthorization(c, h.cfg, expected)
+		if err != nil {
+			return response.ErrorResponse(c, http.StatusForbidden, err.Error())
+		}
+		return next(newCtx)
+	}
+}
+
+func (h *middlewareHandler) PlayerIdParamValidation(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		newCtx, err := h.middlewareUsecase.PlayerParamValidation(c)
 		if err != nil {
 			return response.ErrorResponse(c, http.StatusForbidden, err.Error())
 		}
